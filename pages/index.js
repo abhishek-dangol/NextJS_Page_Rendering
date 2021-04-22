dad8fd65a053951b4b1eb5f6703cf6ed4d6e7efb
Abhishek Dangol <abhi.dangol@gmail.com>
2021-04-21 23:36:05 -0500

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,24 @@
-import { Fragment } from 'react';
-import Head from 'next/head';
-
-import FeaturedPosts from '../components/home-page/featured-posts';
-import Hero from '../components/home-page/hero';
-import { getFeaturedPosts } from '../lib/posts-util';
+import fs from "fs/promises";
+import path from "path";
 
 function HomePage(props) {
+  const { products } = props;
   return (
-    <Fragment>
-      <Head>
-        <title>Max' Blog</title>
-        <meta
-          name='description'
-          content='I post about programming and web development.'
-        />
-      </Head>
-      <Hero />
-      <FeaturedPosts posts={props.posts} />
-    </Fragment>
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
   );
 }
 
-export function getStaticProps() {
-  const featuredPosts = getFeaturedPosts();
-
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
   return {
     props: {
-      posts: featuredPosts,
+      products: data.productsDict,
     },
   };
 }
